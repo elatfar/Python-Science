a153489d2de66d754d51f0b70564dcd99b067b60
Elf-mawt <atmam58@gmail.com>
2019-01-23 15:18:00 +0700

    naive bayes #2

--- Naive Bayes.py
+++ Naive Bayes.py
@@ -11,3 +11,17 @@ def word_feats(words):
 negids = movie_reviews.fileids('neg')
 posids = movie_reviews.fileids('pos')
 
+negfeats = [(word_feats(movie_reviews.words(fileids=[f])),'neg') for f in negids]
+posfeats = [(word_feats(movie_reviews.words(fileids=[f])),'pos') for f in posids]
+
+negcutoff = len(negfeats)*3/4
+poscutoff = len(posfeats)*3/4
+
+trainfeats = negfeats[:negcutoff] + posfeats[:poscutoff]
+testfeats = negfeats[negcutoff:] + posfeats[poscutoff:]
+print 'train on %d instance, test on %d instance'%(len(trainfeats),len(testfeats))
+
+classifier = NaiveBayesClassifier.train(trainfeats)
+print 'accuracy: ', nltk.classify.util.accuracy(classifier,testfeats)
+
+classifier.show_most_informative_features()
